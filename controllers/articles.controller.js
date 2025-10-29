ea4dad23c9e43fbed3a1fab87d472ccf6386e20f
Abhishek Sharma <abhisheksharma@Abhisheks-MacBook-Pro.local>
2025-10-29 15:03:19 +0000

    Add controllers for topics and articles endpoints

---
+++ controllers/articles.controller.js
@@ -0,0 +1,10 @@
+const { selectArticles } = require("../models/articles.model");
+
+exports.getArticles = async (req, res, next) => {
+  try {
+    const articles = await selectArticles();
+    res.status(200).send({ articles });
+  } catch (err) {
+    next(err);
+  }
+};
