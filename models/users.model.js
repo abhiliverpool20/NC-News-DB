6a89b2380ae924f60301beb805db4f4c5ca23092
Abhishek Sharma <abhisheksharma@Abhisheks-MacBook-Pro.local>
2025-10-29 15:20:01 +0000

    Implement GET /api/users endpoint with controller, model, and passing tests

---
+++ models/users.model.js
@@ -0,0 +1,8 @@
+const db = require("../db/connection");
+
+exports.selectUsers = async () => {
+  const { rows } = await db.query(
+    `SELECT username, name, avatar_url FROM users;`
+  );
+  return rows;
+};
