0508285ea96e01a5ceaaac4597241f3428241149
Abhishek Sharma <abhisheksharma@Abhisheks-MacBook-Pro.local>
2025-11-10 14:38:13 +0000

    Update comments controller, DB connection, and .gitignore

--- controllers/comments.controller.js
+++ controllers/comments.controller.js
@@ -1,10 +1,40 @@
-const { selectCommentsByArticleId } = require("../models/comments.model");
+const {
+  selectCommentsByArticleId,
+  insertCommentByArticleId,
+  removeCommentById,
+} = require("../models/comments.model");
 
 exports.getCommentsByArticleId = async (req, res, next) => {
   try {
     const { article_id } = req.params;
     const comments = await selectCommentsByArticleId(article_id);
     res.status(200).send({ comments });
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.postCommentByArticleId = async (req, res, next) => {
+  try {
+    const { article_id } = req.params;
+    const { username, body } = req.body;
+
+    if (!username || !body) {
+      return res.status(400).send({ msg: "Bad request" });
+    }
+
+    const comment = await insertCommentByArticleId(article_id, username, body);
+    res.status(201).send({ comment });
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.deleteCommentById = async (req, res, next) => {
+  try {
+    const { comment_id } = req.params;
+    await removeCommentById(comment_id);
+    res.status(204).send();
   } catch (err) {
     next(err);
   }
--- db/connection.js
+++ db/connection.js
@@ -15,7 +15,7 @@ const config = {};
 if (ENV === "production") {
   config.connectionString = process.env.DATABASE_URL;
   config.max = 2;
-  config.ssl = { rejectUnauthorized: false }; // <-- add this
+  config.ssl = { rejectUnauthorized: false };
 }
 
 const pool = new Pool(config);
